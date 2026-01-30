f12cd64fde485197f81e591037344835ea3328be
charan1147 <charanc1147@gmail.com>
2026-01-30 16:43:54 +0530

    final repo

--- server.js
+++ server.js
@@ -8,16 +8,28 @@ import authRoutes from "./routes/authRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
 import contactRoutes from "./routes/contactRoutes.js";
 import callRoutes from "./routes/callRoutes.js";
-import { setupSocket } from "./websocket/index.js";
+import { Server } from "socket.io";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 const server = http.createServer(app);
-const io = setupSocket(server);
 
-app.use(cors({ origin: process.env.FRONTEND_URL }));
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(helmet());
 
@@ -30,5 +42,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", chatRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/api/call", callRoutes);
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
 
 server.listen(process.env.PORT || 5016, () => console.log("Server running"));
