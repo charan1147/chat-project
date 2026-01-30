2c87ad9e9ef0035d9334cc8db48b2e84ebb8d981
charan1147 <charanc1147@gmail.com>
2026-01-30 17:52:43 +0530

    final repo

--- websocket/index.js
+++ websocket/index.js
@@ -3,7 +3,6 @@ import { Server } from "socket.io";
 export const userSocketMap = new Map();
 
 export const getSocketIdByUserId = (id) => userSocketMap.get(id);
-export const getRoomId = (a, b) => [String(a), String(b)].sort().join("_");
 
 export const setupSocket = (server) => {
   const io = new Server(server, {
@@ -11,27 +10,19 @@ export const setupSocket = (server) => {
   });
 
   io.on("connection", (socket) => {
+    console.log("New connection:", socket.id);
+
     socket.on("register", (userId) => {
       userSocketMap.set(userId, socket.id);
       socket.userId = userId;
-    });
-
-    socket.on("answerCall", ({ roomId, signal }) => {
-      const [a, b] = roomId.split("_");
-      const other = socket.userId === a ? b : a;
-      const socketId = userSocketMap.get(other);
-      if (socketId) io.to(socketId).emit("call:accepted", { signal });
-    });
-
-    socket.on("endCall", ({ roomId }) => {
-      const [a, b] = roomId.split("_");
-      const other = socket.userId === a ? b : a;
-      const socketId = userSocketMap.get(other);
-      if (socketId) io.to(socketId).emit("call:ended");
+      console.log(`Registered user ${userId}`);
     });
 
     socket.on("disconnect", () => {
-      if (socket.userId) userSocketMap.delete(socket.userId);
+      if (socket.userId) {
+        userSocketMap.delete(socket.userId);
+        console.log(`User ${socket.userId} disconnected`);
+      }
     });
   });
 
